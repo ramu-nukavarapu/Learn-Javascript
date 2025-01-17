7bc7f1afe4ad832de0b41fe77b92718f8e845dc6
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-01-17 14:55:54 +0530

    Learn 'Logging' Concept

---
+++ 02 Logging/index.js
@@ -0,0 +1,17 @@
+// Logging is the process of recording messages, errors or information
+// Logging plays a key role whenever we stuck in a many situations (like debugging, monitoring, troubleshooting).
+
+//for that, we use 'console' object available in js
+
+console.log("Hello World!") // .log(message) is method in console object used to log information.
+
+// you can log the information based on their behaviour and level of logging
+
+console.log("This is a log message"); // General information and by default everyone uses it.
+console.info("This is an info message"); // Informational messages
+console.warn("This is a warning message"); // Warnings
+console.error("This is an error message"); // Errors
+console.debug("This is a debug message"); // Debugging details
+
+// Use these methods based on your usecase, to make things as ruled. 
+// To experience the difference b/w the above statements run the 'index.html' in browser and open console on it.
