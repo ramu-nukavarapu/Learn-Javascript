46da2380988370b907d8b5f582eaa3dd0c49c212
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-02-28 20:36:25 +0530

    Learn 'Conditionals' Concept, Updated in Loops folder

--- 04 Loops/index.js
+++ 04 Loops/index.js
@@ -1,3 +1,29 @@
+// Conditional are used to check the condition is true or false. based on the result, executes a particular block
+
+var value = 10
+// simple if - if the conditon is true, the block will execute
+if (value > 5){
+    console.log(value + " greater than 5")
+}
+
+var value = 1
+// if else - if the condition is true, if block is executed otherwise else block is executed
+if (value > 5){
+    console.log(value + " greater than 5")
+}else{
+    console.log(value + " less than 5")
+}
+
+var value = 5
+// if else ladder - check the conditions one after other, if any condition is true then executes the particular block otherwise else block is executed 
+if (value > 5){
+    console.log(value + " greater than 5")
+}else if (value < 5){
+    console.log(value + " less than 5")
+}else{
+    console.log(value+ " equal to 5")
+}
+
 // Loops are used to iterate/repeat a set of statements/instructions
 
 // 1 for loop
